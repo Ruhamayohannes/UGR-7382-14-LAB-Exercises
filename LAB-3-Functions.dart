ecfb956d68c117417b71f6cd5c7b4aeb9dd0f885
Ruhamayohannes <ruhamayohannesm@gmail.com>
2024-03-05 16:30:57 +0300

    edit

--- LAB-3-Functions.dart
+++ LAB-3-Functions.dart
@@ -1,15 +1,17 @@
-void main() {
-  // Example list for testing the function
-  List<num> numbers = [10, 20, 30, 40, 50];
-  
-  double average = calculateAverage(numbers);
-  print("The average is: $average");
+double calculateAverage(List<num> numbers) {
+  if (numbers.isEmpty) {
+    return 0;
+  }
+
+  double sum = 0;
+  for (num number in numbers) {
+    sum += number;
+  }
+  return sum / numbers.length;
 }
 
-// Function to calculate the average of a list of numbers
-double calculateAverage(List<num> numbers) {
-
-  num sum = numbers.reduce((a, b) => a + b);
-  
-  return sum / numbers.length;
+void main() {
+  List<num> numbers = [10, 20, 30, 40, 50];
+  double average = calculateAverage(numbers);
+  print('The average is: $average');
 }
