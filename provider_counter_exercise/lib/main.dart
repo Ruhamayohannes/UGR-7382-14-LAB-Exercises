72e49ad627bf61142a7b6117239d3b9ee9eb3820
Ruhamayohannes <ruhamayohannesm@gmail.com>
2024-04-22 15:27:45 +0300

    edited version

--- provider_counter_exercise/lib/main.dart
+++ provider_counter_exercise/lib/main.dart
@@ -2,88 +2,79 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  runApp(
-    ChangeNotifierProvider(
-      create: (context) => Counter(),
-      child: const MyApp(),
-    ),
-  );
-}
-
-class Counter extends ChangeNotifier {
-  int count = 0;
-
-  void increment() {
-    count++;
-    notifyListeners();
-  }
-
-  void decrement() {
-    count--;
-    notifyListeners();
-  }
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      home: ChangeNotifierProvider(
+        create: (_) => CounterProvider(),
+        child: const MyHomePage(title: 'Flutter Demo '),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
+class CounterProvider extends ChangeNotifier {
+  int _count = 0;
 
-  const MyHomePage({super.key, required this.title});
+  int get count => _count;
+
+  void increment() {
+    _count++;
+    notifyListeners();
+  }
+
+  void decrement() {
+    _count--;
+    notifyListeners();
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(title),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            const Text('You have pushed the button this many times:'),
+            Consumer<CounterProvider>(
+              builder: (context, counterProvider, child) => Text(
+                '${counterProvider.count}',
+                style: Theme.of(context).textTheme.headline6,
+              ),
             ),
-            Consumer<Counter>(
-              builder: (context, count, child) {
-                return Text(
-                  '${count.count}',
-                  style: Theme.of(context).textTheme.headlineMedium,
-                );
-              },
+          ],
+        ),
+      ),
+      floatingActionButton: Consumer<CounterProvider>(
+        builder: (context, counterProvider, child) => Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            FloatingActionButton(
+              onPressed: () => counterProvider.decrement(),
+              tooltip: 'Decrement',
+              child: const Icon(Icons.remove),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                FloatingActionButton(
-                  onPressed:
-                      Provider.of<Counter>(context, listen: false).increment,
-                  tooltip: 'Increment',
-                  child: const Icon(Icons.add),
-                ),
-                const SizedBox(width: 20),
-                FloatingActionButton(
-                  onPressed:
-                      Provider.of<Counter>(context, listen: false).decrement,
-                  tooltip: 'Decrement',
-                  child: const Icon(Icons.minimize),
-                ),
-              ],
+            SizedBox(width: 10),
+            FloatingActionButton(
+              onPressed: () => counterProvider.increment(),
+              tooltip: 'Increment',
+              child: const Icon(Icons.add),
             ),
           ],
         ),
